908e6d0c2f242754c2041d85a973f7ab51d538c0
Young <willy4202@naver.com>
2022-06-13 00:32:47 +0900

    Add: Main Text페이지 컴포넌트화

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,17 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import mainBack from "Images/MainVideo/mainBack.mp4";
+import MainText from "Pages/Main/MainText/";
 
 const Main = () => {
   return (
     <Container>
       <VideoHeader>
-        <Video autoPlay loop>
+        <Video autoPlay muted loop>
           <source src={mainBack} />
         </Video>
       </VideoHeader>
+      <ScrollDiv />
       <Wrapper>
-        <Body>여긴 본문</Body>
+        <MainText />
       </Wrapper>
     </Container>
   );
@@ -21,31 +23,29 @@ export default Main;
 
 const Container = styled.div`
   width: 100vw;
-  height: 200vh;
   position: relative;
-  background-color: black;
 `;
 
 const VideoHeader = styled.div`
-  width: 100%;
   position: fixed;
   top: 0;
-  z-index: -10;
+  z-index: -1;
 `;
 
 const Video = styled.video`
-  width: 100vw;
+  height: 100vh;
+`;
+
+const ScrollDiv = styled.div`
+  width: 100%;
+  height: 100vh;
 `;
 
 const Wrapper = styled.div`
   width: 100vw;
-  margin-top: 1000px;
-  background-color: black;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-const Body = styled.div`
-  width: 1180px;
-  background-color: yellow;
+  background-color: black;
+  color: white;
 `;
