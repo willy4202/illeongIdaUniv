7a54bc90a3c7a1467778a43ce0d95d6722ad58fc
Young <willy4202@naver.com>
2022-06-12 13:54:19 +0900

    Add: image 폴더 생성

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+import mainBack from "../../Images/MainVideo/mainBack.mp4";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <Container>
+      <VideoHeader>
+        <video controls>
+          <source src={mainBack}></source>
+        </video>
+      </VideoHeader>
+      <Body>Mafdsfin</Body>
+    </Container>
+  );
 };
 
 export default Main;
+
+const Container = styled.div`
+  width: 100vw;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const VideoHeader = styled.div`
+  width: 100%;
+`;
+
+const Body = styled.div`
+  width: 1180px;
+  background-color: yellow;
+`;
