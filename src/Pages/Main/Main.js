45b648d3c1a5d63b6da604a3f0b2bbbe906d2489
Young <willy4202@naver.com>
2022-06-12 14:05:27 +0900

    Add: 절대경로 설정

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import mainBack from "../../Images/MainVideo/mainBack.mp4";
+import mainBack from "Images/MainVideo/mainBack.mp4";
 
 const Main = () => {
   return (
