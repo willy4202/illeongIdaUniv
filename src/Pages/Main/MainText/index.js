efac5ad36bc67247aad55eb9faad0d33258f7d66
Young <willy4202@naver.com>
2022-06-13 00:53:51 +0900

    Add: Main 레이아웃 작업 진행중

--- src/Pages/Main/MainText/index.js
+++ src/Pages/Main/MainText/index.js
@@ -4,40 +4,82 @@ import styled from "styled-components";
 const MainText = () => {
   return (
     <Body>
-      <div>
-        [새롭게 일렁이다]는 과거를 기억하고, 이를 현재에 적용하여 더 나은 미래를
-        만드는데 일조하고자 합니다.
-      </div>
-      <div>
-        과거(독립 보훈) - 우리의 선조들이 세상에 삶을 내던져 일었던 '일렁임'을
-        기억합니다.
-      </div>
-      <div>
-        과거(대학가요) - 당시, 대학 대중 문화의 기틀을 만든, 대학가요제를
-        기억합니다.
-      </div>
-      <div>
-        현재(독립 보훈) - 오늘 우리가 발 딛고 사는 이 나라, 이 자유는 대가 없는
-        것이 아닙니다.
-      </div>
-      <div>
-        현재(대학가요) - 사라진 '대중'을 되찾기 위해 멈춰버린 대학가요제의
-        새로운 재계를 도모합니다.
-      </div>
-      <div>
-        미래(독립 보훈) - 이 사실을 모두가 보내는 하루와 그 삶 속에서 자연스레
-        느끼며 호흡할 수 있도록 할 것입니다.
-      </div>
-      <div>
-        미래(대학가요) - 대학가요,재를 통해 새로운 대학 대중문화를 만들 것이며,
-        이를 통해, 새로운 K-문화의 반열에 '대학 대중 문화'가 오를 것입니다.
-      </div>
+      <TextContainer>
+        <LeftText>
+          [새롭게 일렁이다]는
+          <br />
+          과거를 기억하고,
+          <br />
+          이를 현재에 적용하여 <br />더 나은 미래를 만드는데 일조하고자 합니다.
+        </LeftText>
+        <RightText>
+          <div>
+            과거(독립 보훈) <br />- 우리의 선조들이
+            <br /> 세상에 삶을 내던져 일었던
+            <br /> '일렁임'을 기억합니다.
+          </div>
+          <div>
+            과거(대학가요) <br />- 당시, <br />
+            대학 대중 문화의 <br />
+            기틀을 만든, <br />
+            대학가요제를 기억합니다.
+          </div>
+        </RightText>
+        <LeftText>
+          <div>
+            현재(독립 보훈) - <br />
+            오늘 우리가 발 딛고 사는 이 나라, <br />이 자유는 대가 없는 것이
+            아닙니다.
+          </div>
+          <div>
+            현재(대학가요) - <br />
+            사라진 '대중'을 되찾기 위해 <br />
+            멈춰버린 대학가요제의
+            <br />
+            새로운 재계를 도모합니다.
+          </div>
+        </LeftText>
+        <RightText>
+          <div>
+            미래(독립 보훈) - <br />이 사실을 모두가 보내는 하루와 <br />그 삶
+            속에서 자연스레 느끼며 <br />
+            호흡할 수 있도록 할 것입니다.
+          </div>
+          <div>
+            미래(대학가요) - <br />
+            대학가요,재를 통해 새로운 <br />
+            대학 대중문화를 만들 것이며, <br />
+            이를 통해, 새로운 K-문화의 반열에
+            <br />
+            '대학 대중 문화'가 오를 것입니다.
+          </div>
+        </RightText>
+      </TextContainer>
     </Body>
   );
 };
 
 export default MainText;
 const Body = styled.div`
-  width: 1180px;
-  height: 300vh;
+  width: 1120px;
+  justify-content: center;
+`;
+
+const TextContainer = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  white-space: pre-wrap;
+`;
+
+const LeftText = styled.div`
+  text-align: start;
+  font-size: 30px;
+  margin: 40vh 0;
+`;
+const RightText = styled.div`
+  text-align: end;
+  font-size: 30px;
+  margin: 40px;
 `;
