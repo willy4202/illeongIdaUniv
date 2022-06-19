9c498b55e4a6b9687798abd2957143c1eace30fd
Young <willy4202@naver.com>
2022-06-19 12:16:11 +0900

    Add: theme.js, global Font 적용

---
+++ src/Styles/theme.js
@@ -0,0 +1,36 @@
+import { css } from "styled-components";
+
+export const theme = {
+  mainBlack: "#000000",
+  mainWhite: "#FFFFFF",
+  mainPink: "#FF385C",
+  mainGrey: "#F7F7F7",
+  darkGrey: "#797979",
+
+  fontLarge: "48px",
+  fontMedium: "28px",
+  fontSemiMedium: "20px",
+  fontRegular: "18px",
+  fontSmall: "16px",
+  fontMicro: "14px",
+
+  weightBold: 700,
+  weightSemiBold: 600,
+  weightRegular: 400,
+
+  absoluteCenter: css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `,
+};
+
+export const mixins = {
+  flexBox: (direction = "row", align = "center", justify = "center") => `
+    display: flex;
+    flex-direction: ${direction};
+    align-items: ${align};
+    justify-content: ${justify};
+  `,
+};
