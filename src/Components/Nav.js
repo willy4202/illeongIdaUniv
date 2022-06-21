e96212e2cd61987df57ad6a5958c84478ee7abc0
jiho <kkdesign7@naver.com>
2022-06-22 02:59:40 +0900

    nav 1차 가안

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,31 @@
 import React from "react";
+import styledComponents from "styled-components";
+import ilimage from "Images/MainImage/logo.png";
+import "./Nav.css";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <nav className="navbar">
+        <div>
+          <a href="http://localhost:3000/">
+            <img className="nav_logo" src={ilimage} />
+          </a>
+        </div>
+        <ul className="nav_menu">
+          <li>
+            <a href="http://localhost:3000/">HOME</a>
+          </li>
+          <li>
+            <a href="http://localhost:3000/abouts">ABOUTS</a>
+          </li>
+          <li>
+            <a href="/">CONTACT</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
