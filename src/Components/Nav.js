a1369ece7bb33bfc06697ba80cab8237f64c4304
jiho <kkdesign7@naver.com>
2022-06-23 02:54:38 +0900

    nav바 초기

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,31 +1,67 @@
 import React from "react";
-import styledComponents from "styled-components";
+import styled from "styled-components";
 import ilimage from "Images/MainImage/logo.png";
-import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div>
-      <nav className="navbar">
-        <div>
+    <Wrapper>
+      <Navbar>
+        <Logo>
           <a href="http://localhost:3000/">
-            <img className="nav_logo" src={ilimage} />
+            <Navimage src={ilimage} />
           </a>
-        </div>
-        <ul className="nav_menu">
-          <li>
-            <a href="http://localhost:3000/">HOME</a>
-          </li>
-          <li>
-            <a href="http://localhost:3000/abouts">ABOUTS</a>
-          </li>
-          <li>
-            <a href="/">CONTACT</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+        </Logo>
+        <Navmenu>
+          <Navli>
+            <Nava href="http://localhost:3000/">HOME</Nava>
+          </Navli>
+          <Navli>
+            <Nava href="http://localhost:3000/2">SING</Nava>
+          </Navli>
+          <Navli>
+            <Nava href="/">CONTACT US</Nava>
+          </Navli>
+        </Navmenu>
+      </Navbar>
+    </Wrapper>
   );
 };
 
 export default Nav;
+
+const Wrapper = styled.div`
+  background-color: black;
+  height: 60px;
+  width: 100%;
+`;
+
+const Navbar = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  height: 60px;
+`;
+
+const Navimage = styled.img`
+  height: 50px;
+  margin-left: 10px;
+`;
+
+const Navmenu = styled.ul`
+  list-style: none;
+  padding: 10px 12px;
+  display: flex;
+  margin: 2px auto;
+`;
+
+const Navli = styled.li`
+  color: white;
+  margin: 5px auto;
+`;
+
+const Nava = styled.a`
+  text-decoration: none;
+  padding: 5px 40px;
+`;
