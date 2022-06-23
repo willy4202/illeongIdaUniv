167a86db5ce8b0b8cfeff5ede394c7e971c813b3
Young <willy4202@naver.com>
2022-06-23 22:01:51 +0900

    add: 레이아웃 수정

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -12,10 +12,17 @@ const Main = () => {
     AOS.init();
   });
 
+  const size = {
+    width: window.innerWidth || document.body.clientWidth,
+    height: window.innerHeight || document.body.clientHeight,
+  };
+
+  let width = size.width;
+
   return (
     <Container>
-      <MainVideo />
-      <Wrapper>
+      {width > 500 ? <MainVideo /> : null}
+      <Wrapper width={width}>
         <MainText />
         <OurProject />
         <Support />
@@ -32,12 +39,14 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-  width: 100%;
-  padding: 0 20px 300px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: black;
   color: white;
+  width: 100%;
+  margin-top: ${({ width }) => (width > 500 ? "100vh" : "0")};
+
+  padding: 0 20px 300px;
 `;
