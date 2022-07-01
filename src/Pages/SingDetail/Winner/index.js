c19b9bed527233f3993cd7c5d419ee4704dac0d8
jiho <kkdesign7@naver.com>
2022-07-01 16:30:40 +0900

    컴플릭틑 에러

--- src/Pages/SingDetail/Winner/index.js
+++ src/Pages/SingDetail/Winner/index.js
@@ -7,7 +7,7 @@ const Winner = () => {
   return (
     <Container>
 <<<<<<< HEAD
-      <Winnerfirst>
+    <Winnerfirst>
         <Winnerimg src={sing_logo} />
       </Winnerfirst>
       <Youtube>
@@ -24,23 +24,6 @@ const Winner = () => {
       <Supportwrrap>
         <Support />
       </Supportwrrap>
-=======
-      <Wrap>
-        <Winnerimg src={sing_logo} />
-        <Youtube>
-          <iframe
-            width="1320"
-            height="741"
-            src="https://www.youtube.com/embed/SVxiqGiLMCM?start=78"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          />
-        </Youtube>
-        <Support />
-      </Wrap>
->>>>>>> master
     </Container>
   );
 };
@@ -50,7 +33,6 @@ export default Winner;
 const Container = styled.div`
   position: relative;
   @media screen and (max-width: 500px) {
-<<<<<<< HEAD
     width: 320px;
   
 `;
@@ -71,34 +53,12 @@ const Winnerimg = styled.img`
   
   @media screen and (max-width: 500px) {
 
-=======
-    padding: 200px 0;
 `;
 
-const Wrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
-  color: white;
-  width: 100%;
-  margin-top: ${({ width }) => (width > 500 ? "100vh" : "0")};
-  padding: 0 20px 300px;
-`;
-
-const Winnerimg = styled.img`
-  padding-bottom: 20%;
-  @media screen and (max-width: 500px) {
-    width: 140px;
-    height: 140px;
->>>>>>> master
-`;
 
 const Youtube = styled.div`
   position: relative;
   padding-bottom: 10%;
-<<<<<<< HEAD
 
   @media screen and (max-width: 500px) {
     width: 320px;
@@ -116,9 +76,5 @@ height:300px;
 
 const Supportwrrap = styled.div`
   margin-bottom: 200px;
-=======
-  @media screen and (max-width: 500px) {
-    height: 40px;
-  }
->>>>>>> master
+
 `;
